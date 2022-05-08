cdec80f7262b0d612104d1f8af3443f3b426dccf
yajumo93 <yajumo93@gmail.com>
2022-05-08 17:25:16 +0900

    just test

---
+++ D2/test.py
@@ -0,0 +1,11 @@
+print('-'*2)
+
+
+for i in range(0):
+    print('jho')
+    print(i)
+
+tmp_lst = [1, 2, 3]
+
+print(sum(tmp_lst))
+print(max(tmp_lst))
---
+++ test/range_mtd.py
@@ -0,0 +1,13 @@
+
+for i in range(5):
+    print(i, end=' ')
+
+print('\n')
+
+for i in range(1,5):
+    print(i, end=' ')
+
+print('\n')
+
+for i in range(5, 1, -1):
+    print(i, end=' ')
